c33e8b52fdf475cdf1662bbd4d313db54970fcd2
raj-roy <69068408+raj-roy@users.noreply.github.com>
2024-11-01 11:51:54 +0530

    Initialize master

---
+++ ProjectTemplate/TestProjectTemplate/UnitTest1.cs
@@ -0,0 +1,26 @@
+using ProjectTemplate;
+
+namespace TestProjectTemplate2
+{
+    public class UnitTest1
+    {
+        /*Enter test cases here*/
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            
+        }
+
+        [Fact]
+        public void Test3()
+        {
+
+        }
+    }
+}
